14c87a997a92fadcb39f9763975fc6b97df083bf
Sergey Lunkov <s_lunkov@mail.ru>
2021-05-03 15:48:06 +0300

    change struct of folders

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup, find_packages
 setup(
   name='fmods',
   version='0.0.1',
-  packages = find_packages(),
   license="MIT",
   author="Sergey Lunkov",
   package_dir={'fmods': 'src'},
